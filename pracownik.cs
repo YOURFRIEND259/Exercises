9e2910466f64ad07dd33b3573807d75e040ea158
Jakub Borys <kubabor1@gmail.com>
2022-05-26 02:11:49 +0200

    poprawiono czytelnosc w plikach pracownik.cs

--- pracownik.cs
+++ pracownik.cs
@@ -10,7 +10,6 @@
         }
         public void wykaz_pracownikow()
         {
-            Console.WriteLine("Nazwisko:{0} \nZarobki: {1:C}",nazwisko,zarobki);
-            Console.WriteLine();
+            Console.WriteLine("Nazwisko:{0} \nZarobki: {1:C}",nazwisko,zarobki+"\n");
         }
     }
